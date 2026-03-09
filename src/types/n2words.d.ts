68513395a14cc1911892eb3b69f6f4a842922883
vivek <fuubarbaz@gmail.com>
2026-03-09 19:21:33 +0100

    fix: Resolve Metro resolution error by replacing n2words with native TS utility and import correct CSV word list

--- src/types/n2words.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'n2words' {
-    interface N2WordsOptions {
-        lang?: string;
-    }
-    function n2words(number: number | bigint | string, options?: N2WordsOptions): string;
-    export default n2words;
-}
